33411226646ea73e234312659058271479ee2159
João <joaovictor.ramos34@gmail.com>
2023-03-02 12:35:29 -0300

    Criando o quadrado do dia

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,3 +1,5 @@
+import { HabitDay } from "./HabitDay";
+
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 
 export function SummaryTable() {
@@ -14,6 +16,10 @@ export function SummaryTable() {
             </div>
           );
         })}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        <HabitDay />
       </div>
     </div>
   );
