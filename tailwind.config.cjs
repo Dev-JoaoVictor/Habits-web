eb9283b81993a552490a1673154f677bc6a9aa0a
João <joaovictor.ramos34@gmail.com>
2023-03-02 10:53:52 -0300

    Corrigindo o erro das dependencias

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,9 +6,6 @@ module.exports = {
       colors: {
         background: "#09090A",
       },
-      gridTemplateRows: {
-        7: "repeat(7, minmax(0, 1fr))",
-      },
     },
   },
   plugins: [],
