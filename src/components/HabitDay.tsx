d866572760e684a71e162d666896500c19a4bd2f
João <joaovictor.ramos34@gmail.com>
2023-03-05 11:34:50 -0300

    Criando a barra de progresso

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,4 +1,5 @@
 import * as Popover from "@radix-ui/react-popover";
+import { ProgressBar } from "./ProgressBar";
 
 // interface HabitProps {
 //   completed: number;
@@ -10,8 +11,10 @@ export function HabitDay() {
       <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
       <Popover.Portal>
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
-          Teste
-          <Popover.Arrow height={8} width={16} className="fill-zinc-900"/>
+          <span className="font-semibold text-zinc-400 gap">Terça-Feira</span>
+          <span className="font-extrabold leading-tight text-3xl">05/03</span>
+          <ProgressBar progress={75}/>
+          <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
       </Popover.Portal>
     </Popover.Root>
